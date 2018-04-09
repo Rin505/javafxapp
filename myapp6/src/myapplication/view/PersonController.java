34ae2cd3b346c5c8823a4905f173102db0a21f97
rin <rin@testmail.local>
2018-04-09 21:58:05 +0300

    adding myapp6

---
+++ myapp6/src/myapplication/view/PersonController.java
@@ -0,0 +1,62 @@
+package myapplication.view;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import myapplication.MainApp;
+import myapplication.model.Person;
+
+
+public class PersonController {
+    @FXML
+    private TableView<Person> personTable;
+    @FXML
+    private TableColumn<Person, String> firstNameColumn;
+    @FXML
+    private TableColumn<Person, String> lastNameColumn;
+
+    @FXML
+    private Label firstNameLabel;
+    @FXML
+    private Label lastNameLabel;
+    @FXML
+    private Label streetLabel;
+    @FXML
+    private Label cityLabel;
+    @FXML
+    private Label birthdayLabel;
+
+    // Ссылка на главное приложение.
+    private MainApp mainApp;
+
+    /**
+     * Конструктор.
+     * Конструктор вызывается раньше метода initialize().
+     */
+    public PersonController() {
+    }
+
+    /**
+     * Инициализация класса-контроллера. Этот метод вызывается автоматически
+     * после того, как fxml-файл будет загружен.
+     */
+    @FXML
+    private void initialize() {
+        // Инициализация таблицы адресатов с двумя столбцами.
+        firstNameColumn.setCellValueFactory(cellData -> cellData.getValue().firstNameProperty());
+        lastNameColumn.setCellValueFactory(cellData -> cellData.getValue().lastNameProperty());
+    }
+
+    /**
+     * Вызывается главным приложением, которое даёт на себя ссылку.
+     *
+     * @param mainApp
+     */
+    public void setMainApp(MainApp mainApp) {
+        this.mainApp = mainApp;
+
+        // Добавление в таблицу данных из наблюдаемого списка
+        personTable.setItems(mainApp.getPersonData());
+    }
+}
